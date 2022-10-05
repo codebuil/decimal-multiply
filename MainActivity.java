af0c0d361a6086b62de9bc5bc9929e72ad36b989
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-05 10:34:57 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,691 @@
+package multiplicando.app.android;
+
+
+import android.app.*;
+import android.os.*;
+
+
+
+import android.app.*;
+import android.os.*;
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.view.KeyEvent.Callback;
+import 	android.view.InputEvent;
+import	android.view.KeyEvent;
+import android.view.MenuInflater;
+import 	android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
+
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+
+import android.content.Context;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+import java.lang.Object;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import android.text.TextUtils;
+
+import android.view.View; 
+import android.view.View.OnClickListener; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.TextView;
+import java.lang.Object;
+import java.lang.Throwable;
+import java.lang.Exception;
+import java.lang.Exception;
+import java.io.IOException;
+import android.webkit.WebView;
+import android.view.KeyEvent.Callback;
+import java.lang.Object;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+import android.app.Activity;
+import android.view.MenuItem;
+import android.widget.Toast; 
+import android.view.InputEvent;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import java.util.List;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import java.util.AbstractCollection;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import 	android.content.Context;
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+import android.app.Activity;
+import android.app.ListActivity;
+import java.io.*;
+import 
+java.io.InputStream;
+import java.io.FileInputStream;
+
+import 	java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
+import java.util.Arrays;
+import android.util.Base64;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import java.io.RandomAccessFile;
+import 	java.io.RandomAccessFile;
+
+import 	java.nio.charset.Charset;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.SimpleAdapter;
+
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.*;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.*;
+
+import android.graphics.Paint.Style;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+import java.lang.Object;
+import java.util.Random;
+
+import java.lang.Object;
+import android.view.View;
+
+import 
+android.view.ViewGroup.LayoutParams;
+import	android.view.ViewGroup.MarginLayoutParams;
+
+
+
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.view.KeyEvent.Callback;
+import 	android.view.InputEvent;
+import	android.view.KeyEvent;
+import android.view.MenuInflater;
+import 	android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
+
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+
+import android.content.Context;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+import java.lang.Object;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import android.text.TextUtils;
+
+import android.view.View; 
+import android.view.View.OnClickListener; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.TextView;
+import java.lang.Object;
+import java.lang.Throwable;
+import java.lang.Exception;
+import java.lang.Exception;
+import java.io.IOException;
+import android.webkit.WebView;
+import android.view.KeyEvent.Callback;
+import java.lang.Object;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+import android.app.Activity;
+import android.view.MenuItem;
+import android.widget.Toast; 
+import android.view.InputEvent;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import java.util.List;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import java.util.AbstractCollection;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import 	android.content.Context;
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+import android.app.Activity;
+import android.app.ListActivity;
+import java.io.*;
+import 
+java.io.InputStream;
+import java.io.FileInputStream;
+
+import 	java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
+import java.util.Arrays;
+import android.util.Base64;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import java.io.RandomAccessFile;
+import 	java.io.RandomAccessFile;
+
+import 	java.nio.charset.Charset;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.SimpleAdapter;
+
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.*;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.*;
+
+import android.graphics.Paint.Style;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+import java.lang.Object;
+import java.util.Random;
+
+import java.lang.Object;
+//import android.view.View;
+
+import 
+android.view.ViewGroup.LayoutParams;
+import	android.view.ViewGroup.MarginLayoutParams;
+
+import android.app.Application;
+
+import android.content.Context;
+
+
+
+public class MainActivity extends Activity 
+{
+	private Context context;
+	private ListView listview;
+
+	String multiplicars ="0";
+	String por="0";
+	String multiplicarsf ="0";
+	String porf="0";
+	long d1=0L;
+	long d2=0L;
+	long d3=0L;
+
+int ddg=0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private ArrayList<String> string1 = new ArrayList<String>();
+	private ArrayList<String> string2 = new ArrayList<String>();
+
+
+
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+		context = this;
+
+		setTitle("multiplicando");
+
+		int casas1=0;
+		int casas2=0;
+		int casas3=0;
+
+		int ii;
+		boolean b;
+		try
+		{
+			multiplicarsf = System.getProperty("multiplicars.app.android.multiplicar").trim();
+
+			porf = System.getProperty("multiplicars.app.android.por").trim();
+			String porr=porf.replaceAll("'","");
+			String mmult=multiplicarsf.replaceAll("'","");
+			
+			
+			mmult=mmult.replaceAll("\\-","");
+			porr=porr.replaceAll("\\-","");
+			mmult=mmult.replaceAll("\\+","");
+			porr=porr.replaceAll("\\+","");
+			
+			multiplicars=mmult;
+			por=porr;
+			mmult=mmult.replaceAll("\\.","");
+			porr=porr.replaceAll("\\.","");
+			
+			d1 = Long.parseLong(mmult);
+			d2 =  Long.parseLong(porr);
+			
+		
+			
+			
+		}
+		catch (Exception e)
+		{
+
+			d1 = 0L;
+			d2 = 0L;
+			multiplicars = "0";
+			por = "0";
+			multiplicarsf = "0";
+			porf = "0";
+			//setTitle(e.toString());
+			
+		}
+		
+		
+		casas1 = multiplicars.indexOf(".");
+
+		casas2 = por.indexOf(".");
+		
+		
+		if (casas1 < 0)casas1 = 0;
+		else casas1 = multiplicars.length() - casas1 - 1;
+		if (casas2 < 0)casas2 = 0;
+		else casas2 = por.length() - casas2 - 1;
+		
+		multiplicars=multiplicars.replaceAll("\\.","");
+		por=por.replaceAll("\\.","");
+		
+		casas3 = casas1 + casas2;
+		d3 = d1 * d2;
+
+		String multiplicarss=multiplicars;
+		
+		string1.add(String.format("%27s", multiplicarsf));
+		string1.add(String.format("%28s", "X" + porf));
+
+		string1.add("-----------------------------");
+		String h="";
+		String s=func(por).replaceAll("\\.", "");
+
+		long d4=0L;
+		for (int i=0;i < s.length();i++)
+		{
+			String t="" + s.charAt(i);
+			String ss="";
+			try{
+			d4 = Long.parseLong(multiplicarss)  * Long.parseLong(t);
+			ss=(Long.toString(d4) + h);
+			}catch(Exception e){
+				
+				setTitle(e.toString());
+			}
+			ddg=ss.length();
+			ss = ss.replaceAll("-", "");
+			if (s.charAt(i) == '0')
+			{
+				ss = "";
+				
+				for (int f=0;f < multiplicars.length();f++)
+				{
+					ss = ss + "0";
+				}
+				ss = ss + h;
+			}
+			String sss=String.format("%27s", ss);
+			if (casas3 > 0)
+			{
+				String hh=sss.substring(0, sss.length() - casas3);
+				String hh1="." + sss.substring(sss.length() - casas3, sss.length());
+				sss = hh + hh1;
+				sss = String.format("%28s", sss);
+			}
+			 sss=String.format("%28s", sss);
+			string1.add(sss);
+			h = h + "0";
+		}
+		string1.add("-----------------------------");
+		String ss= Long.toString(d3).trim();
+		String sss=String.format("%27s", ss);
+		if (casas3 > 0)
+		{
+			
+			String hh=sss.substring(0, sss.length() - casas3);
+			String hh1="." + sss.substring(sss.length() - casas3, sss.length());
+			sss = hh + hh1;
+			sss = String.format("%28s", sss);
+		}
+		sss = String.format("%28s", sss);
+		if(ddg>16)sss="numero muito grande";
+		string1.add(sss);
+		
+		
+
+
+        listview = (ListView) findViewById(R.id.listview);
+
+        listview.setAdapter(new yourAdaptar(this, string1));
+
+		listview.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+				{
+					// When clicked, show a toast with the TextView text
+
+
+
+
+
+
+
+
+					System.setProperty("multiplicars.app.android.multiplicar", multiplicarsf);
+
+					System.setProperty("multiplicars.app.android.por", porf);
+
+					try
+					{
+						Intent intent = new Intent(context, main2activity.class);
+
+						startActivity(intent);  
+
+						finish(); 
+					}
+					catch (Exception e)
+					{}
+
+				}
+			});
+
+
+
+    }
+
+
+
+
+	private String func(String ss)
+	{
+		int i;
+		String s="";
+		try
+		{
+			for (i = ss.length() - 1;i > -1;i--)
+			{
+				s = s + ss.charAt(i);
+			}
+		}
+		catch (Exception e)
+		{
+			setTitle(e.toString());
+		}
+		return s;
+	}
+
+
+
+}
+
+
+
---
+++ main2activity.java
@@ -0,0 +1,230 @@
+package multiplicando.app.android;
+
+
+
+
+import android.app.*;
+import android.os.*;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.view.KeyEvent.Callback;
+import 	android.view.InputEvent;
+import	android.view.KeyEvent;
+import android.view.MenuInflater;
+import 	android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
+
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+
+import android.content.Context;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
+import java.io.File;
+
+
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+import java.lang.Object;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import android.text.TextUtils;
+
+import android.view.View; 
+import android.view.View.OnClickListener; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.TextView;
+import java.lang.Object;
+import java.lang.Throwable;
+import java.lang.Exception;
+import java.lang.Exception;
+import java.io.IOException;
+import android.webkit.WebView;
+import android.view.KeyEvent.Callback;
+import java.lang.Object;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+import android.app.Activity;
+import android.view.MenuItem;
+import android.widget.Toast; 
+import android.view.InputEvent;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import java.util.List;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import java.util.AbstractCollection;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import 	android.content.Context;
+import android.content.ContextWrapper;
+import android.view.ContextThemeWrapper;
+import android.app.Activity;
+import android.app.ListActivity;
+import java.io.*;
+
+
+import 	java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
+import java.util.Arrays;
+import android.util.Base64;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+
+
+
+
+
+public class main2activity extends Activity 
+{
+	int nn;
+
+	SQLiteDatabase db;
+	Cursor cursor;
+	private Button btn;
+
+	private int iii;
+
+	private Context context;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.adicionar);
+
+		//setTitle();
+		context=this;
+
+		btn = (Button) findViewById(R.id.add); 	
+
+
+		final EditText ED = (EditText)findViewById(R.id.edata);
+
+		final EditText ED2 = (EditText)findViewById(R.id.enome);
+
+
+
+		btn.setOnClickListener(new OnClickListener() { 
+				@Override 
+				public void onClick(View v) {
+					setTitle("calcular");
+
+					System.setProperty("multiplicars.app.android.multiplicar", ED.getText().toString().trim());
+
+					System.setProperty("multiplicars.app.android.por", ED2.getText().toString().trim());
+
+
+
+					Intent intent = new Intent(context,MainActivity.class);
+
+					startActivity(intent);  
+
+
+					finish(); 
+
+
+				}});
+
+
+
+
+
+		setTitle("multiplicar");
+		ED.setText(System.getProperty("multiplicars.app.android.multiplicar"));
+
+		ED2.setText(System.getProperty("multiplicars.app.android.por"));
+
+
+
+	}
+
+
+}
+
+
+
+
